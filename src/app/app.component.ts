6a1e5ec6772dfb677a3698b09ac23be5934bb8e6
Teeradet <tee2490@hotmail.com>
2025-10-16 15:27:15 +0700

    Section02 Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  a = 10;
-  b = 5;
-  ans = this.a + this.b;
+  inputValue: string = 'Hello';
+  isDisabled: boolean = false;
 }
