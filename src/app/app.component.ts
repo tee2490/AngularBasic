113c3faa8a7a8749a9c3d78e04180ef2780c5bd7
Teeradet <tee2490@hotmail.com>
2025-10-17 09:33:13 +0700

    Section02 Two way binding (ngModel)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  counter: number = 0;
-
-  onKeyDown(event: KeyboardEvent) {
-    if (event.key === 'ArrowUp') {
-      this.counter++;
-    } else if (event.key === 'ArrowDown') {
-      this.counter--;
-    }
-  }
+  staticInput: string = 'Static two-way binding';
+  dynamicInput: string = '';
 }
-
-
