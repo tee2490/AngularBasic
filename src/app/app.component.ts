e66deb5418925b0e8606f20009e7a0287fe8864d
Teeradet <tee2490@hotmail.com>
2025-10-17 11:42:10 +0700

    Section03 Control Flow Statements (@if & @else)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  quantity: number = 1;
-  pricePerItem: number = 100;
-
-  get totalPrice(): number {
-    return this.quantity * this.pricePerItem;
-  }
+  num: number = 0;
 }
