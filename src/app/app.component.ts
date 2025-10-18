702931d76caf0ece6134d777dd47066a2fe60ecf
Teeradet <tee2490@hotmail.com>
2025-10-18 13:56:33 +0700

    Section03 Grouping elements with ng-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  nm: string = '';
-  em: string = '';
-  emailIsValid: boolean = false;
-  formSubmitted: boolean = false;
-
-  checkEmailValidation(em: string): void {
-    if (em.includes('@') && em.includes('.com')) {
-      this.emailIsValid = true;
-    } else {
-      this.emailIsValid = false;
-    }
-  }
-  showMessage() {
-    if (this.nm && this.emailIsValid) {
-      this.formSubmitted = true;
-    } else {
-      this.formSubmitted = false;
-    }
-  }
+  subjectList = [
+    {
+      subCode: 101,
+      name: 'JavaScript',
+    },
+    {
+      // subCode: 102,
+      name: 'C++',
+    },
+    {
+      subCode: 103,
+      name: 'NestJS',
+    },
+    {
+      subCode: 104,
+      name: 'Python',
+    },
+  ];
 }
