ef40478bdd43c54a93115204eca60480bb986f4e
Teeradet <tee2490@hotmail.com>
2025-10-18 15:21:42 +0700

    Section03 Portfolio Builder with Defining Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,25 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  listItems: string[] = ['ItemA', 'ItemB', 'ItemC', 'ItemD'];
+  nm = '';
+  em = '';
+  ph = '';
+  add = '';
+  submitted: boolean = false;
+  showHeading: boolean = true;
+  qualification = [{ school: '', degree: '', year: '' }];
+
+  addQualification() {
+    this.qualification.push({ school: '', degree: '', year: '' });
+  }
+
+  formSubmit() {
+    this.submitted = true;
+    this.showHeading = false;
+  }
+
+  formEdit() {
+    this.submitted = false;
+    this.showHeading = true;
+  }
 }
