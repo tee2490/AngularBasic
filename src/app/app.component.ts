a713837936463b521967f9f684d51a9dbefffc7a
Teeradet <tee2490@hotmail.com>
2025-10-19 15:49:37 +0700

    Section05 Property Decorators - @Input()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TestComponent } from './test/test.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, CommonModule, FormsModule, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+export class AppComponent {
+  displayMessage: string = 'Hello, Message from parent component';
 
-export class AppComponent implements OnInit {
-  displayComponent: boolean = true;
-  toggle(): void {
-    this.displayComponent = !this.displayComponent;
-  }
-
-  ngOnInit(): void {
-    console.log('App Component: OnInit');
+  showMsg() {
+    this.displayMessage = 'Child Component: Message Updated';
   }
 }
