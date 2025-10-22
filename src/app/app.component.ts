047291fdc0f7009d20b0f073c80f6053beb3b351
Teeradet <tee2490@hotmail.com>
2025-10-22 12:05:38 +0700

    Section06 Task - Page navigation using slice pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,56 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  sliceArr = ['ItemA', 'ItemB', 'ItemC', 'ItemD'];
+  products: { name: string; imageUrl: string }[] = [
+    {
+      name: 'Mastering CSS with Sass & Bootstrap + Interview Questions',
+      imageUrl: 'assets/Html-CSS.jpg',
+    },
+    {
+      name: 'Mastering Angular + Angular + Interview + E-commerce App ',
+      imageUrl: 'assets/Angular.jpg',
+    },
+    {
+      name: 'JavaScript - Marathon Interview Questions Series',
+      imageUrl: 'assets/JS_Course.png',
+    },
+    {
+      name: 'Mastering React With Interview Questions, eStore Project',
+      imageUrl: 'assets/React_Course.png',
+    },
+    {
+      name: 'Mastering TypeScript with Marathon Interview Questions',
+      imageUrl: 'assets/TypeScript.jpg',
+    },
+    {
+      name: 'Practical Database Guide with RDBMS(MySQL) & NoSQL(MongoDB)',
+      imageUrl: 'assets/RDBMS.png',
+    },
+    {
+      name: 'Mastering Bun - The Modern Fullstack Development',
+      imageUrl: 'assets/Bun.jpg',
+    },
+    {
+      name: 'Mastering NestJS + Interview Question Series',
+      imageUrl: 'assets/NestJS.jpg',
+    },
+  ];
+
+  pgSize: number = 4;
+  startIndex: number = 0;
+  endIndex: number = this.pgSize;
+
+  previousPage() {
+    if (this.startIndex > 0) {
+      this.startIndex -= this.pgSize;
+      this.endIndex -= this.pgSize;
+    }
+  }
+
+  nextPage() {
+    if (this.endIndex < this.products.length) {
+      this.startIndex += this.pgSize;
+      this.endIndex += this.pgSize;
+    }
+  }
 }
