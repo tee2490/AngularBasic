891a040f007b2c7608a19fa5da4df7110145e4df
Teeradet <tee2490@hotmail.com>
2025-10-22 11:41:14 +0700

    Section06 Task - Implementing the built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,11 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-
 export class AppComponent {
-  showDt = new Date()
-  get showDtPlus3(): Date {
-    const d = new Date(this.showDt)
-    d.setFullYear(d.getFullYear() + 5)
-    return d
-  }
+  employees = [
+    { name: 'user A', state: 'california', salary: 50000 },
+    { name: 'user B', state: 'new york', salary: 75000 },
+    { name: 'user C', state: 'texas', salary: 60000 },
+    { name: 'user D', state: 'pennsylvania', salary: 85000 },
+  ];
 }
