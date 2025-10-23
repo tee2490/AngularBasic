ace4901a68d91bcabd2f88c300b53750a4fa0d36
Teeradet <tee2490@hotmail.com>
2025-10-23 09:25:10 +0700

    Section08 Creating A Navigation Link With "routerLink"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  myArray = [1, 2, 3];
-
-  updateValue() {
-    this.myArray.push(5);
-  }
-}
+export class AppComponent {}
