5ed061c30ec64dce1d07ef658e1a1ec8671b647f
Teeradet <tee2490@hotmail.com>
2025-10-23 09:09:37 +0700

    Section08 Creating a simple route - <router-outlet>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SumOfNumbersPipe } from './sum-of-numbers-pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, SumOfNumbersPipe],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
