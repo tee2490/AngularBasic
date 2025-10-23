3d2aec6ae393c962e6c55b4a69c4a6aa756cfa5f
Teeradet <tee2490@hotmail.com>
2025-10-23 10:39:26 +0700

    Section08 Wildcard Routes - Displaying The '404' Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: EmployeeComponent },
-  { path: 'employee/:empName/:empId', component: EmployeeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
