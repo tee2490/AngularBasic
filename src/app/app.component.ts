37bca5be591124611fd6f3ddbacc71240b93150f
Teeradet <tee2490@hotmail.com>
2025-10-23 15:01:09 +0700

    Section09 Introduction To Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { EmployeeService } from './dependencies/employee.service';
+
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule, RouterLink],
+  imports: [RouterOutlet, FormsModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  employees: any[];
+  empId: number = 0;
+  emp: any;
+
+  constructor(private e: EmployeeService) {
+    this.employees = this.e.getEmployees();
+  }
+  showDetails(employeeId: number) {
+    this.empId = employeeId;
+    this.emp = this.e.getEmployeeById(employeeId);
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { CourseDetailComponent } from './course-detail/course-detail.component';
 
-export const routes: Routes = [
-  { path: 'course-detail', component: CourseDetailComponent },
-];
+export const routes: Routes = [];
