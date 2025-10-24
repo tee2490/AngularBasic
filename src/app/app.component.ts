aa583308c31b9f2b209a36867c2624bed2bd3b2b
Teeradet <tee2490@hotmail.com>
2025-10-24 13:34:05 +0700

    Section12 Task - Creating Multi Step Reactive Form With Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+
 import {
-  FormControl,
-  FormGroup,
   ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
   Validators,
 } from '@angular/forms';
 
@@ -15,24 +16,64 @@ import {
 })
 export class AppComponent {
   myForm: FormGroup;
-  nameControl: FormControl;
-  emailControl: FormControl;
+  isSubmitted: boolean = false;
 
-  constructor() {
-    this.nameControl = new FormControl('', Validators.required);
-    this.emailControl = new FormControl('', [
-      Validators.required,
-      Validators.email,
-    ]);
-    this.myForm = new FormGroup({
-      name: this.nameControl,
-      email: this.emailControl,
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      userDetails: this.fb.group({
+        fname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      }),
+      additionalDetails: this.fb.group({
+        mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+        address: ['', Validators.required],
+        country: ['', Validators.required],
+        gender: ['', Validators.required],
+      }),
+      feedback: this.fb.group({
+        comments: [''],
+      }),
     });
+  }
+
+  step: any = 1;
+
+  get userDetails() {
+    return this.myForm.get('userDetails') as FormGroup;
+  }
+
+  get additionalDetails() {
+    return this.myForm.get('additionalDetails') as FormGroup;
+  }
+
+  btnPrevious() {
+    this.step -= 1;
+  }
+
+  btnNext() {
+    const userDetailsGroup = this.myForm.get('userDetails') as FormGroup;
+    const additionalDetailsGroup = this.myForm.get(
+      'additionalDetails'
+    ) as FormGroup;
+
+    if (userDetailsGroup.invalid && this.step == 1) {
+      return;
+    }
+
+    if (additionalDetailsGroup.invalid && this.step == 2) {
+      return;
+    }
+
+    if (this.step < 3) {
+      this.step += 1;
+    }
   }
 
   formSubmit() {
     if (this.myForm.valid) {
-      console.log('Form Values: ', this.myForm.value);
+      this.isSubmitted = true;
     }
+    console.log(this.myForm.value);
   }
 }
