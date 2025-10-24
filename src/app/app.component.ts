a75a3110163ab2c53046ebe4c9d955c85bc060c3
Teeradet <tee2490@hotmail.com>
2025-10-24 13:45:58 +0700

    Section13 Tracking Form Control States

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,64 +16,17 @@ import {
 })
 export class AppComponent {
   myForm: FormGroup;
-  isSubmitted: boolean = false;
 
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
-      userDetails: this.fb.group({
-        fname: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', Validators.required],
-      }),
-      additionalDetails: this.fb.group({
-        mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
-        address: ['', Validators.required],
-        country: ['', Validators.required],
-        gender: ['', Validators.required],
-      }),
-      feedback: this.fb.group({
-        comments: [''],
-      }),
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
-  }
-
-  step: any = 1;
-
-  get userDetails() {
-    return this.myForm.get('userDetails') as FormGroup;
-  }
-
-  get additionalDetails() {
-    return this.myForm.get('additionalDetails') as FormGroup;
-  }
-
-  btnPrevious() {
-    this.step -= 1;
-  }
-
-  btnNext() {
-    const userDetailsGroup = this.myForm.get('userDetails') as FormGroup;
-    const additionalDetailsGroup = this.myForm.get(
-      'additionalDetails'
-    ) as FormGroup;
-
-    if (userDetailsGroup.invalid && this.step == 1) {
-      return;
-    }
-
-    if (additionalDetailsGroup.invalid && this.step == 2) {
-      return;
-    }
-
-    if (this.step < 3) {
-      this.step += 1;
-    }
   }
 
   formSubmit() {
     if (this.myForm.valid) {
-      this.isSubmitted = true;
+      console.log(this.myForm.value);
     }
-    console.log(this.myForm.value);
   }
 }
