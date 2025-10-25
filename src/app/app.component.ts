85eeb329175ff5f862871b528140cc589e424e84
Teeradet <tee2490@hotmail.com>
2025-10-25 12:02:37 +0700

    Section14 CanActivate Route Guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,12 @@
 import { Component } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { urlValidator } from './custom-validators/url-validator';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterOutlet],
 })
-export class AppComponent {
-  validateURL: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.validateURL = this.fb.group({
-      checkURL: ['', [Validators.required, urlValidator]],
-    });
-  }
-  validate() {
-    if (!this.validateURL.valid) {
-      alert('Please enter correct URL');
-      return false;
-    } else {
-      return alert('URL is valid');
-    }
-  }
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { testGuard } from './guards/test-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin', component: AdminComponent, canActivate: [testGuard] },
+];
