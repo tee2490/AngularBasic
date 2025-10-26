d9aae7686143bb485c8f7f82c89f0b21a2874025
Teeradet <tee2490@hotmail.com>
2025-10-26 16:49:30 +0700

    Section17 RxJS { of } Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule],
 })
 export class AppComponent {
-  randomNum = Math.floor(Math.random() * 90) + 10;
-  coldObservable$ = new Observable<number>((observer) => {
-    observer.next(this.randomNum);
-    observer.complete();
-  });
-
   constructor() {
-    this.coldObservable$.subscribe({
-      next: (value) => console.log(`Subscriber 1: ${value}`),
-    });
-    this.coldObservable$.subscribe({
-      next: (value) => console.log(`Subscriber 2: ${value}`),
-    });
-
-    setTimeout(() => {
-      this.coldObservable$.subscribe({
-        next: (value) => console.log(`Subscriber 3: ${value}`),
-      });
-    }, 2000);
+    const observable$ = of(
+      { name: 'User', age: 27 },
+      [1, 2, 3, 4],
+      () => 'Hello'
+    );
+    observable$.subscribe((val) => console.log(val));
   }
 }
