9de81a89365ac2536f794aa6740deee08b18ec0d
Teeradet <tee2490@hotmail.com>
2025-10-26 14:19:51 +0700

    Section16 Managing Observables & Subscribers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,34 @@ import { Observable } from 'rxjs';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [],
 })
 export class AppComponent {
+  observable = new Observable((observer) => {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.error(5);
+
+    setTimeout(() => {
+      console.log('---After two seconds---');
+      observer.next(4);
+      observer.complete();
+    }, 2000);
+  });
+
   constructor() {
-    const observable = new Observable((observer) => {
-      console.log('Observable executed');
-    }).subscribe();
+    console.log('Before subscribe');
+    this.observable.subscribe({
+      next(x) {
+        console.log('Value: ', x);
+      },
+      error(e) {
+        console.error('Error occurred: ', e);
+      },
+      complete() {
+        console.log('Observable successfully executed');
+      },
+    });
+    console.log('After subscribe');
   }
 }
