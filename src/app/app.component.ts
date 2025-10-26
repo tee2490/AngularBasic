4a237248cae101ae6206adad214d201d762af84c
Teeradet <tee2490@hotmail.com>
2025-10-26 19:57:51 +0700

    section17 RxJS { map } Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { filter, from } from 'rxjs';
+import { fromEvent, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    const obs$ = from([
-      { name: 'XYZ', age: 25 },
-      { name: 'ABC', age: 35 },
-    ]);
+  cursorPosition$?: Observable<{ x: number; y: number }>;
 
-    const filterAge$ = obs$.pipe(filter((person) => person.age >= 30));
-    filterAge$.subscribe((val) =>
-      console.log(`Age above 30: \nName: ${val.name}, Age: ${val.age}`)
+  ngOnInit(): void {
+    this.cursorPosition$ = fromEvent<MouseEvent>(window, 'mousemove').pipe(
+      map((e) => ({ x: e.clientX, y: e.clientY }))
     );
   }
 }
