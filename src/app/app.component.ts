51bbfb947808b6027b072f2e14410e7a3571554a
Teeradet <tee2490@hotmail.com>
2025-10-27 10:41:15 +0700

    Section18 Modifying Data With PUT Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule],
 })
 export class AppComponent {
   constructor(private http: HttpClient) {}
 
-  postData() {
-    const user = {
-      id: 102,
-      name: 'UserB',
-      email: 'userb@gmail.com',
-    };
+  putData() {
     this.http
-      .post('http://localhost:3000/users', user)
-      .subscribe((res) => console.log('User updated: ', res));
+      .get<any>('http://localhost:3000/users/102')
+      .subscribe((updatedData) => {
+        (updatedData.name = 'NewUserD'),
+          (updatedData.email = 'newuserd@gmail.com');
+        this.http
+          .put('http://localhost:3000/users/102', updatedData)
+          .subscribe({
+            next: (res) => console.log('User Updated: ', res),
+            error: (err) => console.error('Error updating the data: ', err),
+          });
+      });
   }
 }
