bd62b3718bf1ee7b6527488ddf35606d4e0c6d56
Teeradet <tee2490@hotmail.com>
2025-10-27 10:52:07 +0700

    Section18 Removing Data With DELETE Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: './app.component.css',
+  imports: [FormsModule],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {}
+  http = inject(HttpClient);
+  userId: string = '';
 
-  putData() {
-    this.http
-      .get<any>('http://localhost:3000/users/102')
-      .subscribe((updatedData) => {
-        (updatedData.name = 'NewUserD'),
-          (updatedData.email = 'newuserd@gmail.com');
-        this.http
-          .put('http://localhost:3000/users/102', updatedData)
-          .subscribe({
-            next: (res) => console.log('User Updated: ', res),
-            error: (err) => console.error('Error updating the data: ', err),
-          });
-      });
+  deleteData() {
+    if (!this.userId) {
+      alert('Please enter a valid user id');
+      return;
+    }
+
+    this.http.delete(`http://localhost:3000/users/${this.userId}`).subscribe({
+      next: () => {
+        console.log('Data deleted successfully...');
+      },
+      error: (err) => {
+        console.error('Something went wrong :(', err);
+      },
+    });
   }
 }
